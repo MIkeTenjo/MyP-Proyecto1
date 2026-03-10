2ec1d8543bc4bb852e379290f61ce5b95a0469bb
Josè Luis Huaxcuahutli Teopantzin <joseluisht@ciencias.unam.mx>
2026-03-10 11:29:50 -0600

    empezando

---
+++ app/bin/main/com/modelado/proyecto1/chat/Main.kt
@@ -0,0 +1,9 @@
+package com.modelado.proyecto1.chat
+
+class Main {
+    fun run() {
+        println("¡Hola Mundo!")
+        println("La clase Main se ha inicializado desde su propio archivo.")
+        println("Listo para empezar a programar el chat_textoo...")
+    }
+}
